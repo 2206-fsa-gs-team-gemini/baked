077f63a1b8ff5ba4287a46bf594e88c7e2f6cd0e
nickyjhong <nicolehong95@gmail.com>
2022-08-09 11:55:54 -0400

    admin only routes

--- client/Routes.js
+++ client/Routes.js
@@ -1,57 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { withRouter, Route, Switch } from 'react-router-dom';
 import { Login } from './components/AuthForm';
 import Home from './components/Home';
 import { me } from './store';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
-import CreateUser from './components/CreateUser';
 import SingleProduct from './components/SingleProduct';
 import OrderHistory from './components/OrderHistory';
 import UserProfile from './components/UserProfile';
-import Admin from './components/Admin';
-import Checkout from './components/Checkout';
+import CreateProduct from './components/CreateProduct';
+import CreateUser from './components/CreateUser';
 
-/**
- * COMPONENT
- */
 class Routes extends Component {
   componentDidMount() {
     this.props.loadInitialData();
-    // this.props.fetchUsers();
   }
 
   render() {
     const { isLoggedIn, isAdmin } = this.props;
-    console.log(this.props.getAllUsers);
-    console.log(this.props.state)
+
     return (
       <div>
         {isLoggedIn ? (
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/products" component={Shop} />
-            <Route exact path="/products/:id" component={SingleProduct} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/cart/checkout/:cartId" component={Checkout} />
-            <Route exact path="/profile" component={UserProfile} />
-            <Route path="/users/orders" component={OrderHistory} />
-
-            {isAdmin && <Route exact path='/login/admin' component={Admin} />}
-            
+          <div>
             {isAdmin ? (
-              <Redirect from='/login' to='/login/admin' />
-            ) : (
-              <Redirect from='/login' to='/' />
+              <Switch> {/* Routes if logged in and admin */}
+                <Route exact path="/" component={Home} />
+                <Route exact path="/products" component={Shop} />            
+                <Route path ='/products/add' component={CreateProduct} />
+                <Route path="/products/:id" component={SingleProduct} />
+                <Route exact path="/cart" component={Cart} />
+                <Route exact path="/profile" component={UserProfile} />
+                <Route path="/users/orders" component={OrderHistory} />
+              </Switch>
+            )  : (
+              <Switch> {/* Routes if logged in but not admin */}
+                <Route exact path="/" component={Home} />
+                <Route exact path="/products" component={Shop} />            
+                <Route exact path="/products/:id" component={SingleProduct} />
+                <Route exact path="/cart" component={Cart} />
+                <Route exact path="/profile" component={UserProfile} />
+                <Route path="/users/orders" component={OrderHistory} />
+              </Switch>
             )}
-            
-          </Switch>
+          </div>
         ) : (
-          <Switch>
+          <Switch> {/* Routes if not logged in */}
             <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={CreateUser} />
             <Route exact path="/products" component={Shop} />
@@ -72,8 +68,7 @@ const mapState = (state) => {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
     isLoggedIn: !!state.auth.id,
-    isAdmin: state.auth.isAdmin == 'TRUE',
-    state: state.allUsers,
+    isAdmin: !!state.auth.isAdmin
   };
 };
 
@@ -82,7 +77,6 @@ const mapDispatch = (dispatch) => {
     loadInitialData() {
       dispatch(me());
     },
-    getAllUsers: () => dispatch(fetchUsers()),
   };
 };
 
