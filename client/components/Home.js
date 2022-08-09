2c99d3abeea8447d6f9fc40b9ccea118e4e57d88
Amy <amyannnwong@gmail.com>
2022-08-09 16:52:59 -0400

    update on home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 export const Home = (props) => {
   const { firstName } = props;
+  if (!firstName) {
+    return (
+      <div>
+        <div className="welcome">Hello!</div>
+      </div>
+    );
+  }
   return (
-  <div>
-    <div className="welcome">
-      Hello {firstName}
+    <div>
+      <div className="welcome">Hi {firstName}!</div>
     </div>
-  <div className='home-images'>
-    <img src="" />
-  </div>
-  </div>);
+  );
 };
 
 const mapState = (state) => {
