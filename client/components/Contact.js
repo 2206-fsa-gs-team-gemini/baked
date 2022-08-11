d1597440a5633ab2d90447f0c3d0d03c2718a965
Amy <amyannnwong@gmail.com>
2022-08-11 19:07:28 -0400

    Contact page

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// export class Contact extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <section className="grid-section">Contact</section>
+//
+
+import React from "react";
+import { Link } from "react-router-dom";
 
 export class Contact extends React.Component {
   render() {
     return (
       <div>
-        <section className="grid-section">Contact</section>
-        <article className="contact">
+        <section className="contact-header">Contact</section>
           <p className="contact-desc">
             Obviously, Baked By Fullstack isn't a real bakery and unfortunately,
             we don't sell any of the products we have listed.
@@ -16,143 +25,124 @@ export class Contact extends React.Component {
             contacted!
           </p>
           <br />
-          <br />
 
-          <h3 className="contact-desc">Our Bakers:</h3>
-          <div className="team-member">
-            <img
-              src="https://avatars.githubusercontent.com/u/101421813?v=4"
-              className="team-member-image"
-            ></img>
-            <h4 className="team-member-name">Nicole Hong</h4>
-            <Link
-              to={{ pathname: 'https://github.com/nickyjhong' }}
-              target="_blank"
-              className="team-member-link"
-            >
-              Github
-            </Link>
-            <br />
-            <Link
-              to={{
-                pathname: 'https://www.linkedin.com/in/nicolejhong/',
-              }}
-              target="_blank"
-              className="team-member-link"
-            >
-              LinkedIn
-            </Link>
-            <br />
-            <br />
-          </div>
+        <p className="bakers">The Bakers</p>
+        <div className="members">
+          <div className="container">
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src="https://avatars.githubusercontent.com/u/101421813?v=4" />
+                </div>
+                <div className="contentBx">
+                  <h4>Nicole Hong</h4>
+                </div>
+                <div className="sci">
+                    <Link
+                      to={{ pathname: "https://github.com/nickyjhong" }}
+                      target="_blank"
+                    >
+                      Github
+                    </Link>
+                    <br />
+                    <Link
+                      to={{
+                        pathname: "https://www.linkedin.com/in/nicolejhong/",
+                      }}
+                      target="_blank"
+                    >
+                      LinkedIn
+                    </Link>
+                </div>
+              </div>
+            </div>
 
-          <div className="team-member">
-            <img
-              src="https://avatars.githubusercontent.com/u/41312534?v=4"
-              className="team-member-image"
-            ></img>
-            <h4 className="team-member-name">Cherry Xu</h4>
-            <Link
-              to={{ pathname: 'https://github.com/mscherryxu' }}
-              target="_blank"
-              className="team-member-link"
-            >
-              Github
-            </Link>
-            <br />
-            <Link
-              to={{
-                pathname: 'https://www.linkedin.com/in/cherry-xu-rd-cdn/',
-              }}
-              target="_blank"
-              className="team-member-link"
-            >
-              LinkedIn
-            </Link>
-            <br />
-            <br />
-          </div>
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src="https://avatars.githubusercontent.com/u/41312534?v=4" />
+                </div>
+                <div className="contentBx">
+                  <h4>Cherry Xu</h4>
+                </div>
+                <div className="sci">
+                    <Link
+                      to={{ pathname: "https://github.com/mscherryxu" }}
+                      target="_blank"
+                    >
+                      Github
+                    </Link>
+                    <br />
+                    <Link
+                      to={{
+                        pathname:
+                          "https://www.linkedin.com/in/cherry-xu-rd-cdn/",
+                      }}
+                      target="_blank"
+                    >
+                      LinkedIn
+                    </Link>
+                </div>
+              </div>
+            </div>
 
-          <div className="team-member">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C4D03AQF40Wuek0HEcw/profile-displayphoto-shrink_200_200/0/1584683546842?e=1665619200&v=beta&t=0paII6E8NHIhjjOqwt7ppmuwPcmHltZW8WLrzZN-5po"
-              className="team-member-image"
-            ></img>
-            <h4 className="team-member-name">Amy Wong</h4>
-            <Link
-              to={{ pathname: 'https://github.com/amyawong' }}
-              target="_blank"
-              className="team-member-link"
-            >
-              Github
-            </Link>
-            <br />
-            <Link
-              to={{
-                pathname: 'https://www.linkedin.com/in/amyawong/',
-              }}
-              target="_blank"
-              className="team-member-link"
-            >
-              LinkedIn
-            </Link>
-            <br />
-            <br />
-          </div>
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF40Wuek0HEcw/profile-displayphoto-shrink_200_200/0/1584683546842?e=1665619200&v=beta&t=0paII6E8NHIhjjOqwt7ppmuwPcmHltZW8WLrzZN-5po" />
+                </div>
+                <div className="contentBx">
+                  <h4>Amy Wong</h4>
+                </div>
+                <div className="sci">
+                    <Link
+                      to={{ pathname: "https://github.com/amyawong" }}
+                      target="_blank"
+                    >
+                      Github
+                    </Link>
+                    <br />
+                    <Link
+                      to={{
+                        pathname: "https://www.linkedin.com/in/amyawong/",
+                      }}
+                      target="_blank"
+                    >
+                      LinkedIn
+                    </Link>
+                </div>
+              </div>
+            </div>
 
-          <div className="team-member">
-            <img
-              src="https://media-exp1.licdn.com/dms/image/C4E03AQEQ-OfyTk-7cg/profile-displayphoto-shrink_200_200/0/1561991819331?e=1665619200&v=beta&t=O_-Y2Z2Jnn3XmnTRAmNM8E7rFjEvmTgDras8XMyIXMc"
-              className="team-member-image"
-            ></img>
-            <h4 className="team-member-name">Chris Tomshack</h4>
-            <Link
-              to={{ pathname: 'https://github.com/Ctomshack' }}
-              target="_blank"
-              className="team-member-link"
-            >
-              Github
-            </Link>
-            <br />
-            <Link
-              to={{
-                pathname: 'https://www.linkedin.com/in/christomshack/'
-              }}
-              target="_blank"
-              className="team-member-link"
-            >
-              LinkedIn
-            </Link>
-            <br />
-            <br />
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEQ-OfyTk-7cg/profile-displayphoto-shrink_200_200/0/1561991819331?e=1665619200&v=beta&t=O_-Y2Z2Jnn3XmnTRAmNM8E7rFjEvmTgDras8XMyIXMc" />
+                </div>
+                <div className="contentBx">
+                  <h4>Chris Tomshack</h4>
+                </div>
+                <div className="sci">
+                    <Link
+                      to={{ pathname: "https://github.com/Ctomshack" }}
+                      target="_blank"
+                    >
+                      Github
+                    </Link>
+                    <br />
+                    <Link
+                      to={{
+                        pathname: "https://www.linkedin.com/in/christomshack/",
+                      }}
+                      target="_blank"
+                    >
+                      LinkedIn
+                    </Link>
+                </div>
+              </div>
+            </div>
           </div>
-          {/* <h3 className="contact-desc">Special Thanks To:</h3>
-          <div className="team-member">
-            <img src="" className="team-member-image"></img>
-            <h4 className="team-member-name"></h4>
-            <Link
-              to={{ pathname: "https://github.com/" }}
-              target="_blank"
-              className="team-member-link"
-            >
-              Github
-            </Link>
-            <br />
-            <Link
-              to={{
-                pathname: "https://www.linkedin.com/in/",
-              }}
-              target="_blank"
-              className="team-member-link"
-            >
-              LinkedIn
-            </Link>
-            <p></p>
-          </div> */}
-        </article>
-        <br />
-        <br />
-        <br />
+        </div>
       </div>
     );
   }
