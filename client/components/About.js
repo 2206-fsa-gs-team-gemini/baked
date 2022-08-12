43868f002344489c5b3d7ffaa4171bbcc91992b6
Amy <amyannnwong@gmail.com>
2022-08-11 23:09:46 -0400

    About page transitions not working

--- client/components/About.js
+++ client/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export class About extends React.Component {
   render() {
     return (
       <div>
-        <section className="grid-section">About</section>
-        <article className="about">
-          <p>
+        {/* <section className="contact-header">About</section>
+        <article className="contact-desc">
+          <p className="about-p">
             Thanks for stopping by! Here at Baked By Fullstack, we guarantee top
             quality products at affordable prices. Our products are freshly
             baked everyday with quality as our top priority. Each baked good is
@@ -17,79 +18,77 @@ export class About extends React.Component {
           <br />
           <br />
         </article>
-        <h4>Our Secret Recipe 🤫</h4>
+        <p className="secret-recipe">Our Secret Recipe 🤫</p>
+        <p className="s-r-2">Instructions can be found <Link to={{ pathname: "https://github.com/2206-fsa-gs-team-gemini/baked" }} target="_blank" className="footer-link">here</Link> or in the <span style={{ color: '#d49090' }}>Recipe</span> tab on the bottom of the page</p> */}
+        <div className="slidershow">
+          <section id="slider">
+            <input type="radio" name="slider" id="s1" checked />
+            <input type="radio" name="slider" id="s2" />
+            <input type="radio" name="slider" id="s3" />
+            <input type="radio" name="slider" id="s4" />
+            <input type="radio" name="slider" id="s5" />
+            <input type="radio" name="slider" id="s6" />
+            <input type="radio" name="slider" id="s7" />
+            <input type="radio" name="slider" id="s8" />
 
-        <div className="slidershow middle">
-          <div className="slides">
-            <input type="radio" name="r" id="r1" checked />
-            <input type="radio" name="r" id="r2" />
-            <input type="radio" name="r" id="r3" />
-            <input type="radio" name="r" id="r4" />
-            <input type="radio" name="r" id="r5" />
-            <input type="radio" name="r" id="r6" />
-            <input type="radio" name="r" id="r7" />
-            <input type="radio" name="r" id="r8" />
-
-            <div className="slide">
+            <label for="s1" id="slide1">
               <img
+                className="js-logo"
+                alt="javascript"
                 src="https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s2" id="slide2">
               <img
+                className="react-js-logo"
+                alt="react js"
                 src="https://d12i7q49526cmu.cloudfront.net/media/original_images/React_Logo.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s3" id="slide3">
               <img
+                className="redux-js-logo"
+                alt="redux js"
                 src="https://seeklogo.com/images/R/redux-logo-9CA6836C12-seeklogo.com.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s4" id="slide4">
               <img
+                className="express-js-logo"
+                alt="express js"
                 src="https://res.cloudinary.com/practicaldev/image/fetch/s--KkScstnJ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/zojuy79lo3fn3qdt7g6p.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s5" id="slide5">
               <img
+                className="sequelize-logo"
+                alt="sequelize"
                 src="https://www.vectorlogo.zone/logos/sequelizejs/sequelizejs-ar21.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s6" id="slide6">
               <img
+                className="postgres-logo"
+                alt="postgreSQL"
                 src="https://www.unixmen.com/wp-content/uploads/2017/07/postgresql-logo.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s7" id="slide7">
               <img
+                className="css-logo"
+                alt="css"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png"
-                className="secret-recipe"
               />
-            </div>
-            <div className="slide">
+            </label>
+            <label for="s8" id="slide8">
               <img
+                className="html-logo"
+                alt="html"
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS9v00g4XP1X0sFzxp64FIBSIgchtoRkAZSj_fjzq75u16gd3RcOmWYHJazTTHc6WSt30&usqp=CAU"
-                className="secret-recipe"
               />
-            </div>
-          </div>
-
-          <div className="navigation">
-            <label for="r1" className="bar"></label>
-            <label for="r2" className="bar"></label>
-            <label for="r3" className="bar"></label>
-            <label for="r4" className="bar"></label>
-            <label for="r5" className="bar"></label>
-            <label for="r6" className="bar"></label>
-            <label for="r7" className="bar"></label>
-            <label for="r8" className="bar"></label>
-          </div>
+            </label>
+          </section>
+          <br /><br /><br />
         </div>
       </div>
     );
